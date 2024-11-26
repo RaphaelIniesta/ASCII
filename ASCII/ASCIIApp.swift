116892720ba21916f471a472a04f560248e63551
RaphaelIniesta <raphael_iniesta@yahoo.com>
2024-11-26 12:39:03 -0300

    Initial Commit

---
+++ ASCII/ASCIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCIIApp.swift
+//  ASCII
+//
+//  Created by Raphael Iniesta Reis on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ASCIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASCII/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASCII
+//
+//  Created by Raphael Iniesta Reis on 26/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
